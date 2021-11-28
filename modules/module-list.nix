33b9999899bb62074895fcfa383fd0e9305054eb
John Soo <john.soo@arista.com>
2021-11-28 15:45:22 -0800

    nix-serve: Initial introduction.
    This provides a similar interface as in nixpkgs.

--- modules/module-list.nix
+++ modules/module-list.nix
@@ -86,6 +86,7 @@
   ./services/monitoring/prometheus-node-exporter.nix
   ./services/netbird.nix
   ./services/nix-daemon.nix
+  ./services/nix-serve.nix
   ./services/nix-gc
   ./services/nix-optimise
   ./services/ofborg
