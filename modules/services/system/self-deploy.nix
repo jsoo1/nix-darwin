8b29a183d7fb12455da28340d93bf7272af7cca7
John Soo <jsoo1@asu.edu>
2022-02-24 07:12:07 -0700

    self-deploy: Fix PATH.

--- modules/services/system/self-deploy.nix
+++ modules/services/system/self-deploy.nix
@@ -22,7 +22,7 @@ let
   script = pkgs.writeShellScript "self-deploy" ''
     ${lib.optionalString (builtins.isString cfg.input.sshKeyFile)
       ''export GIT_SSH_COMMAND="${pkgs.openssh}/bin/ssh -i ${lib.escapeShellArg cfg.input.sshKeyFile}"''}
-    export PATH=${pkgs.gnutar}/bin:${pkgs.gzip}:$PATH
+    export PATH=${pkgs.gnutar}/bin:${pkgs.gzip}/bin:$PATH
 
     if [ ! -e ${repositoryDirectory} ]; then
       mkdir -p ${repositoryDirectory}
